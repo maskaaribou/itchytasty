37d5952ca10193b4dd1c0b6d1e728de7af98e9bb
maskaaribou <maskaaribou@gmail.com>
2026-09-24 19:56:44 +0200

    Update script.js

--- gifs/script.js
+++ gifs/script.js
@@ -437,7 +437,7 @@ function createCard(gif) {
 
         setTimeout(() => {
             card.classList.remove("clicked");
-        }, 200);
+        }, 300);
 
         try {
             await navigator.clipboard.writeText(gif.GIF);
